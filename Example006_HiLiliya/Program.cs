40b316279ed28688306ae0b7700a11b32716031a
Adalberto777 <albert.tliashev@gmail.com>
2022-08-18 19:59:08 +0500

    Exaple006_HiLiliya

---
+++ Example006_HiLiliya/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What's is your name?");
+string username = Console.ReadLine();
+if (username.ToLower() == "liliya")
+{
+    Console.Write("Hello Liliya");
+}
+else
+{
+    Console.Write("Who are you? ");
+}
