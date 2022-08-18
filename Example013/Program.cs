68992681358b89a0b32039dd384eb03c73272243
Adalberto777 <albert.tliashev@gmail.com>
2022-08-19 00:01:11 +0500

    Exaple013

---
+++ Example013/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write namber of the day");
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1) Console.Write("понедельник");
+if(number == 2) Console.Write("вторник");
+if(number == 3) Console.Write("среда");
+if(number == 4) Console.Write("четверг");
+if(number == 5) Console.Write("пятница");
+if(number == 6) Console.Write("суббота");
+if(number == 7) Console.Write("воскресение");
