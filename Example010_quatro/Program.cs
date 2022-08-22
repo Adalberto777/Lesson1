6d613f0442a1d0bb3f3432196b90c2924c86b418
Adalberto777 <albert.tliashev@gmail.com>
2022-08-22 23:34:02 +0500

    Example114 Проверка кратности

--- Example010_quatro/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("Write namber");
-int number = int.Parse(Console.ReadLine());
-Console.Write($"квадрат числа {number}={number*number}");
---
+++ Example114/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число для проверки кратности: ");
+int num1 = int.Parse (Console.ReadLine());
+Console.Write("Введите первое число для сравнения: ");
+int even1 = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число для сравнения: ");
+int even2 = int.Parse (Console.ReadLine());
+
+if (num1 % even1 == 0 && num1 % even2 == 0)
+{
+    Console.WriteLine(num1 + " - является кратным" + " " + even1 + " и " + even2);
+}
+else
+{
+    Console.WriteLine(num1 + " - не является кратным" + " " + even1 + " и " + even2);
+}
+
