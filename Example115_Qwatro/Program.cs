26edceecee2a800ea08efbbe6fd9ed3f0de672fb
Adalberto777 <albert.tliashev@gmail.com>
2022-08-22 23:48:10 +0500

    Example115_Qwatro

---
+++ Example115_Qwatro/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число");
+int num1 = int.Parse (Console.ReadLine());
+Console.Write("Введите число ");
+int num2 = int.Parse (Console.ReadLine());
+
+if (num1 == num2*num2 )
+{
+    Console.WriteLine($"{num1} квадратом {num2} ");
+}
+
+if (num2 == num1*num1 )
+{
+    Console.WriteLine($"{num2} квадратом {num1} ");
+}
